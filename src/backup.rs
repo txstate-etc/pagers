f7a1efb9d9c4148167b0a587a436fc331ff68b6b
Charles Tabor <chastabor@gmail.com>
2018-06-08 16:50:28 -0500

    percent encoded the backup filename

--- src/backup.rs
+++ src/backup.rs
@@ -1,8 +1,13 @@
 use nodes::PathInfo;
+use percent_encoding::{utf8_percent_encode, PATH_SEGMENT_ENCODE_SET};
 
-// Turn PathInfo into /<repo>/<site>-<archive_extension>/<repo>.<site.path>.xml
-pub fn name(site_info: &PathInfo, path_info: &PathInfo, archive_extension: &str) -> String {
-    format!("/{}{}-{}/{}{}.xml", site_info.repo_type, site_info.path, archive_extension, path_info.repo_type, path_info.path.replace("/", "."))
+// Turn PathInfo into /<repo>/<site>-<archive_extension>/percent_encoded(<repo>/<site/path>).xml
+pub fn name(site: &PathInfo, path: &PathInfo, archive_extension: &str) -> String {
+    format!("/{}{}-{}/{}.xml",
+        site.repo_type,
+        site.path,
+        archive_extension,
+        utf8_percent_encode(&format!("{}{}", path.repo_type, path.path), PATH_SEGMENT_ENCODE_SET))
 }
 
 
@@ -20,10 +25,10 @@ mod tests {
             last_modified: None,
         };
         let path = PathInfo{
-            path: "/gato/subpage1/subpage2/file".to_string(),
+            path: "/gato/subpage1/subpage2/file name.odf".to_string(),
             repo_type: RepoType::Website,
             last_modified: Some("2018-05-05T08:59:29.261-05:00".parse::<DateTime<Local>>().unwrap()),
         };
-        assert_eq!(name(&site, &path, "20180506"), "/website/gato-20180506/website.gato.subpage1.subpage2.file.xml");
+        assert_eq!(name(&site, &path, "20180506"), "/website/gato-20180506/website%2Fgato%2Fsubpage1%2Fsubpage2%2Ffile%20name.odf.xml");
     }
 }
